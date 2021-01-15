6c955e1a90ebed748c7b3f9b0d48004301a62983
ervin <ervinhodzic1234@gmail.com>
2021-01-15 23:40:50 +0100

    movie

--- movie.py
+++ movie.py
@@ -113,7 +113,7 @@ def result():
       #movies1 = np.array(q_movies.loc[q_movies['genres'] == data1][['title', 'genres']].head(20))
       #stocklist = list(movies.flatten())
       #print(movies)
-      return render_template("test.html",movies=movies, result = handle, personality = user)
+      return render_template("result.html",movies=movies, result = handle, personality = user)
 
 
 
